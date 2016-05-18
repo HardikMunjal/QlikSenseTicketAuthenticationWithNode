28b8cb8ae4834c5105751fb31ebff3393e6d0a17
hardi <hardik.munjaal@gmail.com>
2016-05-19 00:18:19 +0530

    qlik auth node modules

--- routes/index.js
+++ routes/index.js
@@ -4,4 +4,7 @@ module.exports = function (app) {
  app.get('/', function(req, res, next) {
 			    res.render('login/login.html');
 			  });
+  app.get('/authenticate', function(req, res, next) {
+			    res.render('login/login.html');
+			  });
 };
