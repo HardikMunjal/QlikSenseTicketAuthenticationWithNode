6bc14476c5186061e350b2ecca91988fb1b3e67d
hardi <hardik.munjaal@gmail.com>
2016-05-20 17:21:15 +0530

    add certification and node qlik roots

--- public/scripts/loginScript.js
+++ public/scripts/loginScript.js
@@ -7,28 +7,24 @@ app.controller('myCtrl', function($scope, $http) {
 		$scope.myTxt = "You clicked submit!";
 
 
-    //  $http.get("/loginapi")
-    // .then(function(response) {
-    //     $scope.myWelcome = response.data;
-    // });
+     $http.get("http://localhost/loginapi")
+    .then(function(response) {
+        $scope.myWelcome = response.data;
+    });
 
-    $scope.data = { "name" : "mohit",
-    "password" : "password4",
-    "profession" : "teacher",
-    "id": 4
-}
+   
      console.log($scope.username);
      var formData = {};
      formData.username = $scope.username;
      formData.password = $scope.password;
 
 
-$http.post("/loginapi", formData).success(function(data, status) {
-	$scope.loginResponse = data.message;
-	//uncoment server/hub and comment localhost/authenticate
-	//location.href = "http://10.2.5.160/hub/";
-    location.href = "http://localhost:4000/authenticate";
-})
+// $http.post("/loginapi", formData).success(function(data, status) {
+// 	$scope.loginResponse = data.message;
+// 	//uncoment server/hub and comment localhost/authenticate
+// 	location.href = "http://10.2.5.160/hclnode/hub/";
+//     //location.href = "http://localhost:4000/authenticate";
+// })
 }
 
 });
--- routes/index.js
+++ routes/index.js
@@ -1,63 +1,79 @@
 module.exports = function (app) {
 
 //https://community.qlik.com/thread/160788
- var qlikauth = require('qlik-auth');
+var qlikauth = require('qlik-auth');
 
- app.get('/', function(req, res, next) {
-			    res.render('login/login.html');
-			  });
- 
+app.get('/', function(req, res, next) {
+	res.render('login/login.html');
+});
 
-	app.get('/loginapi', function(req, res, next) {
 
-		var result=
-		{ "name" : "mohit",
-		"password" : "password4",
-		"profession" : "teacher",
-		"id": 4
-	   }
+app.get('/loginapi', function(req, res, next) {
+
+  console.log(req.headers.origin);
+	var result=
+	{ "name" : "mohit",
+	"password" : "password4",
+	"profession" : "teacher",
+	"id": 4
+    }
+
+res.json(result);
+});
+
+app.post('/loginapi', function(req, res, next) {
+
+
+  console.log(req.body);
+  if(req.body.username=='qlikdeveloper5' && req.body.password=='123'){
+
+	var result=  {  
+		 			"message" : "success",
+					"username" : "qlikdeveloper5",
+					"id": 4123
+				 }
 
 	res.json(result);
-    });
-
-	app.post('/loginapi', function(req, res, next) {
-
-		
-	console.log(req.body);
+	console.log('nice bro');
 
 
-	if(req.body.username=='hardi_123' && req.body.password=='123'){
 
-		var result=
-			{ "message" : "success",
-			"username" : "hardi_123",
-			"id": 4123
-		    }
-		    res.json(result);
-			console.log('nice bro');
-	  }else{
-	    var result=
-			{ "message" : "reject",
-			"username" : "hardi_123",
-			"id": 4123
-		    }
-		    res.json(result);
-			console.log('bad bro');
-		}
+	}else{
 
-   });
+	 var result={
+					"message" : "reject",
+					"username" : "qlikdeveloper5",
+					"id": 4123
+				}
 
-		app.get('/authenticate', function(req, res, next) {
-			  var profile = {
-			    'UserDirectory': 'QLIK', 
-			    'UserId': 'rikard',
-			    'Attributes': [{'Group': 'ExampleGroup'}]
-			  }
+	res.json(result);
+	console.log('bad bro');
+	}
 
-			  //Make call for ticket request
-			  //qlikauth.requestTicket(req, res, profile);
-			  res.render('index.html');
-		});
+});
 
- };
+
+
+app.get('/authenticate', function(req, res, next) {
+	
+	var profile = {
+		'UserDirectory': 'ASSOCIATES', 
+		'UserId': 'qlikdeveloper5'
+		// ,
+		// 'Attributes': [{'Group': 'ExampleGroup'}]
+	 }
+    console.log('coming to authenticate');
+
+    //Make call for ticket request
+	qlikauth.requestTicket(req, res, profile);
+	res.render('index.html');
+  });
+
+
+app.get('/page', function(req, res, next) {
+	
+	
+	res.render('index.html');
+  });
+};
 
